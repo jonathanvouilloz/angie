3edf4af802a3ccc35283b4d95c652f664186dbe9
Jon <jonathan.vouilloz@gmail.com>
2025-12-10 00:58:32 +0100

    external link + blog post automatisation club 2025

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,17 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import sitemap from '@astrojs/sitemap';
+import rehypeExternalLinks from 'rehype-external-links';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jonlabs.ch',
   integrations: [sitemap()],
+  markdown: {
+    rehypePlugins: [
+      [rehypeExternalLinks, { target: '_blank', rel: ['noopener', 'noreferrer'] }]
+    ]
+  },
   vite: {
     plugins: [tailwindcss()],
   },
